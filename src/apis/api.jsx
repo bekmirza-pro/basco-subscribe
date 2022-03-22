25bb8a938f13a60edc11f998348af7c0ba856bc3
YoqubovBekmirza <bekmirzaprogrammer@yandex.ru>
2022-03-22 05:48:03 -0700

    url commit

--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://31.42.189.4:16333/webcabinet/hs/webcab"
+  baseURL: "https://fazliddin.online/webcabinet/hs/webcab"
 });
